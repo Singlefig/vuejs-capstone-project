8c497ecee3c18e49c608a57f0f39012403ae6a79
Singlefig <45048082+Singlefig@users.noreply.github.com>
2022-08-17 16:16:11 +0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <nav>
+  <Header />
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
+  </nav> -->
   <router-view />
 </template>
+
+<script>
+import Header from './components/Header.vue'
+export default {
+  components: {
+    Header,
+  },
+}
+</script>
 
 <style>
 #app {
@@ -15,16 +25,7 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  margin: 0;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <div class="routes">
+      <router-link to="/">Characters</router-link>
+      <router-link to="/favourites">Favourites</router-link>
+    </div>
+    <div>
+      <img
+        src="../assets/icons/heart.svg"
+        width="32"
+        height="32"
+        alt="icon for favourite"
+      />
+      <span>1</span>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Header',
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #000;
+  width: 100%;
+}
+
+.routes {
+  display: flex;
+  column-gap: 12px;
+}
+</style>
